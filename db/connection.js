0fd444f3ccc4fbec409e0c50ed7c5b6f9b012245
senachary <senachary1@gmail.com>
2023-08-10 22:22:45 +0100

    adds parks table

--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,13 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+
+const { Pool } = require("pg");
+
+if (!process.env.PGDATABASE) {
+  throw new Error("no PGDATABASE env var");
+}
+
+const connection = new Pool();
+
+module.exports = connection;
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 const { parks, rides, stalls } = require("./data/index.js");
-
+const format = require("pg-format");
 const db = require("./connection");
 
 function seed() {
@@ -24,7 +24,14 @@ function seed() {
 
 function createParks() {
   /* Create your parks table in the query below */
-  return db.query("");
+  const createParksTable = `
+  CREATE TABLE parks(
+    park_id SERIAL PRIMARY KEY,
+      park_name VARCHAR NOT NULL,
+      year_opened INT NOT NULL,
+      annual_attendance INT NOT NULL
+  );`;
+  return db.query(createParksTable);
 }
 
 module.exports = seed;
